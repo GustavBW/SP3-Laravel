c67d0c5fe09165f8250b07ac1da24b707480f4de
Alexander <algle11@student.sdu.dk>
2022-12-12 13:34:13 +0100

    IngredientsController

--- app/Http/Controllers/IngredientsController.php
+++ app/Http/Controllers/IngredientsController.php
@@ -8,16 +8,31 @@ use App\Http\Controllers\OPCClientController;
 
 class IngredientsController extends Controller
 {
-    function ingredientsFill(Request $request) {
-        
-        $value = $object->barley();
+    function ingredientsFill()
+    {
+        // Create an instance of the OPCClientController class
+        $opcClient = new OPCClientController();
+
+        // Get the response from the getInventoryStatus() method
+        $response = $opcClient->getInventoryStatus();
+
+        // Use the json_decode() function to convert the JSON string into a PHP object
+        $responseData = json_decode($response);
+
+        // Create an Ingredients object and initialize its properties using the values from the response data
         $ingredients = new Ingredients([
-            'barley' => $request->input("Barley")->input("value"),
-            'hops' => $request->input("Hops")->input("value"),
-            'yeast' => $request->input("Yeast")->input("value"),
-            'malt' => $request->input("Malt")->input("value"),
-            'wheat' => $request->input("Wheat")->input("value")
+            'barley' => $responseData->barley,
+            'hops' => $responseData->hops,
+            'yeast' => $responseData->yeast,
+            'malt' => $responseData->malt,
+            'wheat' => $responseData->wheat,
         ]);
         $ingredients->save();
+
+        return $ingredients;
     }
 }
+
+//to get individuel values from the ingredients object use ->
+// $ingredients->barley or $ingredients->hops   etc.
+
