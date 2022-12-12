5dc69ac4fd80a33cd7029e1de22a982936fb387e
Kim JE Lassen <kimjelassen@yahoo.com>
2022-12-12 12:03:11 +0100

    Initial Commit

---
+++ .history/app/Http/Controllers/IngredientsController_20221212115941.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IngredientsController extends Controller
+{
+    function ingredientsFill() {
+        $ingredients = new Ingredients([
+            'barley' => $request->barley,
+            'hops' => $request->hops,
+            'yeast' => $request->yeast,
+            'malt' => $request->malt,
+            'wheat' => $request->wheat
+        ]);
+        $ingredients->save();
+        
+    }
+}
---
+++ .history/app/Http/Controllers/IngredientsController_20221212120147.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IngredientsController extends Controller
+{
+    function ingredientsFill() {
+        $nodes = OPCClientController::getInventoryStatus();
+        $ingredients = new Ingredients([
+            'barley' => $nodes->barley,
+            'hops' => $nodes->hops,
+            'yeast' => $nodes->yeast,
+            'malt' => $nodes->malt,
+            'wheat' => $nodes->wheat
+        ]);
+        $ingredients->save();
+    }
+}
