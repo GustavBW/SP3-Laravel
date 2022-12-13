fd6b61c5cb18e079211a85e471a826c966cd8a33
Alexander <algle11@student.sdu.dk>
2022-12-13 12:24:26 +0100

    Moved stuff around and laughed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,123 +7,21 @@ use App\Models\User;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $user = User::all();
-        return view('users.index', compact('user'));
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    //Create user
     public function create()
     {
         return view('users.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    //Store created user
     public function store(Request $request)
     {
-        $user = new User([
+        User::create([
             'name' => $request->name,
-            'email' => $request->email,
             'password' => $request->password,
-            'pin' => $request->pin
         ]);
-        $user->save();
+
         return redirect('user');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $user = User::find($id);
-        return view('users.show', compact('user'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $user = User::find($id);
-        return view('users.edit', compact('user'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $user = User::find($id);
-        $user->update([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => $request->password,
-            'pin' => $request->pin
-        ]);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $user = User::find($id);
-        $user->delete();
-        return redirect('user');
-    }
-
-
-            /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function verifyPin(Request $request,$id)
-    {
-        //
-    }
-
-        /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  String  $id
-     * @param  String  $password
-     * @return \Illuminate\Http\Response
-     */
-    public function verifyUser(Request $request  ,$id, $password)
-    {
-        //
     }
 }
